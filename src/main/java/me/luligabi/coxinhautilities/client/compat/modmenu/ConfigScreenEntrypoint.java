5efccfcdf8c0076ed915fb2425fa4dbb22272acb
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2022-11-07 21:33:58 -0300

    Fix server startup crash, bump mod version to 1.4.5

---
+++ src/main/java/me/luligabi/coxinhautilities/client/compat/modmenu/ConfigScreenEntrypoint.java
@@ -0,0 +1,134 @@
+package me.luligabi.coxinhautilities.client.compat.modmenu;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import dev.isxander.yacl.api.ConfigCategory;
+import dev.isxander.yacl.api.Option;
+import dev.isxander.yacl.api.OptionGroup;
+import dev.isxander.yacl.api.YetAnotherConfigLib;
+import dev.isxander.yacl.gui.controllers.BooleanController;
+import dev.isxander.yacl.gui.controllers.slider.IntegerSliderController;
+import me.luligabi.coxinhautilities.common.CoxinhaUtilities;
+import me.luligabi.coxinhautilities.common.ModConfig;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class ConfigScreenEntrypoint implements ModMenuApi {
+
+    @Override
+    public ConfigScreenFactory<Screen> getModConfigScreenFactory() {
+        return this::createConfigScreen;
+    }
+
+    private Screen createConfigScreen(Screen parent) {
+        ModConfig config = CoxinhaUtilities.CONFIG;
+
+        /*
+         * Ender Orchid
+         */
+        Option<Integer> enderOrchidSpawnRate = Option.createBuilder(Integer.class)
+                .name(Text.translatable("configOption.coxinhautilities.enderOrchidSpawnRate"))
+                .tooltip(Text.translatable("configOption.coxinhautilities.enderOrchidSpawnRate.tooltip"))
+                .binding(
+                        2,
+                        () -> config.enderOrchidSpawnRate,
+                        newValue -> config.enderOrchidSpawnRate = newValue
+                )
+                .available(config.canGenerateEnderOrchids)
+                .controller((intOption) -> new IntegerSliderController(intOption, 2, 20, 2))
+                .build();
+
+        Option<Boolean> canGenerateEnderOrchids = Option.createBuilder(Boolean.class)
+                .name(Text.translatable("configOption.coxinhautilities.canGenerateEnderOrchids"))
+                .tooltip(Text.translatable("configOption.coxinhautilities.canGenerateEnderOrchids.tooltip"))
+                .binding(
+                        true,
+                        () -> config.canGenerateEnderOrchids,
+                        newValue -> {
+                            config.canGenerateEnderOrchids = newValue;
+                            enderOrchidSpawnRate.setAvailable(newValue);
+                        }
+                )
+                .controller((booleanOption) -> new BooleanController(booleanOption, BooleanController.YES_NO_FORMATTER, true))
+                .build();
+
+        Option<Integer> enderOrchidSpecialGrowthRate = Option.createBuilder(Integer.class)
+                .name(Text.translatable("configOption.coxinhautilities.enderOrchidSpecialGrowthRate"))
+                .tooltip(Text.translatable("configOption.coxinhautilities.enderOrchidSpecialGrowthRate.tooltip"))
+                .binding(
+                        12,
+                        () -> config.enderOrchidSpecialGrowthRate,
+                        newValue -> config.enderOrchidSpecialGrowthRate = newValue
+                )
+                .available(!config.hasEnderOrchidStrictPlacement)
+                .controller((intOption) -> new IntegerSliderController(intOption, 2, 20, 2))
+                .build();
+
+        Option<Boolean> hasEnderOrchidStrictPlacement = Option.createBuilder(Boolean.class)
+                .name(Text.translatable("configOption.coxinhautilities.hasEnderOrchidStrictPlacement"))
+                .tooltip(Text.translatable("configOption.coxinhautilities.hasEnderOrchidStrictPlacement.tooltip"))
+                .binding(
+                        true,
+                        () -> config.hasEnderOrchidStrictPlacement,
+                        newValue -> {
+                            config.hasEnderOrchidStrictPlacement = newValue;
+                            enderOrchidSpecialGrowthRate.setAvailable(!newValue);
+                        }
+
+                )
+                .controller((booleanOption) -> new BooleanController(booleanOption, BooleanController.YES_NO_FORMATTER, true))
+                .build();
+
+        Option<Integer> enderOrchidRegularGrowthRate = Option.createBuilder(Integer.class)
+                .name(Text.translatable("configOption.coxinhautilities.enderOrchidRegularGrowthRate"))
+                .tooltip(Text.translatable("configOption.coxinhautilities.enderOrchidRegularGrowthRate.tooltip"))
+                .binding(
+                        8,
+                        () -> config.enderOrchidRegularGrowthRate,
+                        newValue -> config.enderOrchidRegularGrowthRate = newValue
+                )
+                .controller((intOption) -> new IntegerSliderController(intOption, 2, 20, 2))
+                .build();
+
+        /*
+         * Cardboard Box
+         */
+        Option<Boolean> useCarrierBlacklist = Option.createBuilder(Boolean.class)
+                .name(Text.translatable("configOption.coxinhautilities.useCarrierBlacklist"))
+                .tooltip(Text.translatable("configOption.coxinhautilities.useCarrierBlacklist.tooltip"))
+                .binding(
+                        true,
+                        () -> config.useCarrierBlacklist,
+                        newValue -> config.useCarrierBlacklist = newValue
+                )
+                .controller((booleanOption) -> new BooleanController(booleanOption, BooleanController.YES_NO_FORMATTER, true))
+                .build();
+
+        return YetAnotherConfigLib.createBuilder()
+                .title(Text.translatable("itemGroup.coxinhautilities.item_group"))
+                .category(ConfigCategory.createBuilder()
+                        .name(Text.translatable("block.coxinhautilities.ender_orchid"))
+                        .group(OptionGroup.createBuilder()
+                                .name(Text.translatable("block.coxinhautilities.ender_orchid"))
+
+                                .option(canGenerateEnderOrchids)
+                                .option(enderOrchidSpawnRate)
+                                .option(hasEnderOrchidStrictPlacement)
+                                .option(enderOrchidRegularGrowthRate)
+                                .option(enderOrchidSpecialGrowthRate)
+                                .build())
+                        .build())
+                .category(ConfigCategory.createBuilder()
+                        .name(Text.translatable("block.coxinhautilities.cardboard_box"))
+                        .group(OptionGroup.createBuilder()
+                                .name(Text.translatable("block.coxinhautilities.cardboard_box"))
+
+                                .option(useCarrierBlacklist)
+                                .build())
+                        .build())
+                .save(() -> CoxinhaUtilities.saveConfig(config))
+                .build()
+                .generateScreen(parent);
+    }
+
+}
--- src/main/java/me/luligabi/coxinhautilities/common/ModConfig.java
+++ src/main/java/me/luligabi/coxinhautilities/common/ModConfig.java
@@ -1,14 +1,5 @@
 package me.luligabi.coxinhautilities.common;
 
-import dev.isxander.yacl.api.ConfigCategory;
-import dev.isxander.yacl.api.Option;
-import dev.isxander.yacl.api.OptionGroup;
-import dev.isxander.yacl.api.YetAnotherConfigLib;
-import dev.isxander.yacl.gui.controllers.BooleanController;
-import dev.isxander.yacl.gui.controllers.slider.IntegerSliderController;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
-
 public class ModConfig {
 
     // Ender Orchid
@@ -26,114 +17,6 @@ public class ModConfig {
     public boolean useCarrierBlacklist = true;
 
 
-    public Screen createGui(Screen parent) {
 
-        /*
-         * Ender Orchid
-         */
-        Option<Integer> enderOrchidSpawnRate = Option.createBuilder(Integer.class)
-                .name(Text.translatable("configOption.coxinhautilities.enderOrchidSpawnRate"))
-                .tooltip(Text.translatable("configOption.coxinhautilities.enderOrchidSpawnRate.tooltip"))
-                .binding(
-                        2,
-                        () -> this.enderOrchidSpawnRate,
-                        newValue -> this.enderOrchidSpawnRate = newValue
-                )
-                .available(this.canGenerateEnderOrchids)
-                .controller((intOption) -> new IntegerSliderController(intOption, 2, 20, 2))
-                .build();
-
-        Option<Boolean> canGenerateEnderOrchids = Option.createBuilder(Boolean.class)
-                .name(Text.translatable("configOption.coxinhautilities.canGenerateEnderOrchids"))
-                .tooltip(Text.translatable("configOption.coxinhautilities.canGenerateEnderOrchids.tooltip"))
-                .binding(
-                        true,
-                        () -> this.canGenerateEnderOrchids,
-                        newValue -> {
-                            this.canGenerateEnderOrchids = newValue;
-                            enderOrchidSpawnRate.setAvailable(newValue);
-                        }
-                )
-                .controller((booleanOption) -> new BooleanController(booleanOption, BooleanController.YES_NO_FORMATTER, true))
-                .build();
-
-        Option<Integer> enderOrchidSpecialGrowthRate = Option.createBuilder(Integer.class)
-                .name(Text.translatable("configOption.coxinhautilities.enderOrchidSpecialGrowthRate"))
-                .tooltip(Text.translatable("configOption.coxinhautilities.enderOrchidSpecialGrowthRate.tooltip"))
-                .binding(
-                        12,
-                        () -> this.enderOrchidSpecialGrowthRate,
-                        newValue -> this.enderOrchidSpecialGrowthRate = newValue
-                )
-                .available(!this.hasEnderOrchidStrictPlacement)
-                .controller((intOption) -> new IntegerSliderController(intOption, 2, 20, 2))
-                .build();
-
-        Option<Boolean> hasEnderOrchidStrictPlacement = Option.createBuilder(Boolean.class)
-                .name(Text.translatable("configOption.coxinhautilities.hasEnderOrchidStrictPlacement"))
-                .tooltip(Text.translatable("configOption.coxinhautilities.hasEnderOrchidStrictPlacement.tooltip"))
-                .binding(
-                        true,
-                        () -> this.hasEnderOrchidStrictPlacement,
-                        newValue -> {
-                            this.hasEnderOrchidStrictPlacement = newValue;
-                            enderOrchidSpecialGrowthRate.setAvailable(!newValue);
-                        }
-
-                )
-                .controller((booleanOption) -> new BooleanController(booleanOption, BooleanController.YES_NO_FORMATTER, true))
-                .build();
-
-        Option<Integer> enderOrchidRegularGrowthRate = Option.createBuilder(Integer.class)
-                .name(Text.translatable("configOption.coxinhautilities.enderOrchidRegularGrowthRate"))
-                .tooltip(Text.translatable("configOption.coxinhautilities.enderOrchidRegularGrowthRate.tooltip"))
-                .binding(
-                        8,
-                        () -> this.enderOrchidRegularGrowthRate,
-                        newValue -> this.enderOrchidRegularGrowthRate = newValue
-                )
-                .controller((intOption) -> new IntegerSliderController(intOption, 2, 20, 2))
-                .build();
-
-        /*
-         * Cardboard Box
-         */
-        Option<Boolean> useCarrierBlacklist = Option.createBuilder(Boolean.class)
-                .name(Text.translatable("configOption.coxinhautilities.useCarrierBlacklist"))
-                .tooltip(Text.translatable("configOption.coxinhautilities.useCarrierBlacklist.tooltip"))
-                .binding(
-                        true,
-                        () -> this.useCarrierBlacklist,
-                        newValue -> this.useCarrierBlacklist = newValue
-                )
-                .controller((booleanOption) -> new BooleanController(booleanOption, BooleanController.YES_NO_FORMATTER, true))
-                .build();
-
-        return YetAnotherConfigLib.createBuilder()
-                .title(Text.translatable("itemGroup.coxinhautilities.item_group"))
-                .category(ConfigCategory.createBuilder()
-                        .name(Text.translatable("block.coxinhautilities.ender_orchid"))
-                        .group(OptionGroup.createBuilder()
-                                .name(Text.translatable("block.coxinhautilities.ender_orchid"))
-
-                                .option(canGenerateEnderOrchids)
-                                .option(enderOrchidSpawnRate)
-                                .option(hasEnderOrchidStrictPlacement)
-                                .option(enderOrchidRegularGrowthRate)
-                                .option(enderOrchidSpecialGrowthRate)
-                                .build())
-                        .build())
-                .category(ConfigCategory.createBuilder()
-                        .name(Text.translatable("block.coxinhautilities.cardboard_box"))
-                        .group(OptionGroup.createBuilder()
-                                .name(Text.translatable("block.coxinhautilities.cardboard_box"))
-
-                                .option(useCarrierBlacklist)
-                                .build())
-                        .build())
-                .save(() -> CoxinhaUtilities.saveConfig(this))
-                .build()
-                .generateScreen(parent);
-    }
 
 }
