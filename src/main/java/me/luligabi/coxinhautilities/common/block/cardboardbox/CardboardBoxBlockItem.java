9afdaa56ce9f143dbe26aa2c1fb76046f6042432
Luligabi1 <33578169+Luligabi1@users.noreply.github.com>
2023-05-25 00:53:23 -0300

    Closes #21

--- src/main/java/me/luligabi/coxinhautilities/common/block/cardboardbox/CardboardBoxBlockItem.java
+++ src/main/java/me/luligabi/coxinhautilities/common/block/cardboardbox/CardboardBoxBlockItem.java
@@ -23,6 +23,8 @@ import net.minecraft.util.Clearable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.Optional;
+
 public class CardboardBoxBlockItem extends BlockItem {
 
     public CardboardBoxBlockItem() {
@@ -32,34 +34,37 @@ public class CardboardBoxBlockItem extends BlockItem {
     @SuppressWarnings("ConstantConditions")
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
-        World world = context.getWorld();
-        BlockPos pos = context.getBlockPos();
-        BlockEntity blockEntity = world.getBlockEntity(pos);
-        if(!world.isClient()) {
-            if(context.getPlayer().isSneaking() && blockEntity != null) {
+        if(!context.getWorld().isClient()) {
+            World world = context.getWorld();
+            BlockPos pos = context.getBlockPos();
+            Optional<BlockEntity> blockEntity = Optional.ofNullable(world.getBlockEntity(pos));
+
+            if(context.getPlayer().isSneaking()) {
                 BlockState blockState = world.getBlockState(pos);
-                if(blockState.getBlock().getHardness() >= 0.01F && isNbtBlockAir(context.getStack()) &&
-                        !blockState.isIn(TagRegistry.UNBOXABLE) && !isOnCarrierBlackList(blockState) && !hasLootTable(blockEntity)) {
+                if(blockState.getBlock().getHardness() >= 0.01F && isNbtBlockAir(context.getStack()) && !blockState.isIn(TagRegistry.UNBOXABLE) && !isOnCarrierBlackList(blockState)) {
+                    if(blockEntity.isPresent() && hasLootTable(blockEntity.get())) return super.useOnBlock(context);
 
                     NbtList nbtList = new NbtList();
-                    NbtCompound nbtCopy = blockEntity.createNbtWithId();
-                    nbtCopy.remove("id");
-                    nbtCopy.remove("x");
-                    nbtCopy.remove("y");
-                    nbtCopy.remove("z");
-                    nbtList.add(nbtCopy);
+                    if(blockEntity.isPresent()) {
+                        NbtCompound nbtCopy = blockEntity.get().createNbtWithId();
+                        nbtCopy.remove("id");
+                        nbtCopy.remove("x");
+                        nbtCopy.remove("y");
+                        nbtCopy.remove("z");
+                        nbtList.add(nbtCopy);
 
-                    // Desperate attempt to cover every edge case :)
-                    Clearable.clear(blockEntity);
-                    world.removeBlockEntity(pos);
-
+                        // Desperate attempt to cover every edge case :)
+                        Clearable.clear(blockEntity);
+                        world.removeBlockEntity(pos);
+                    }
                     world.setBlockState(pos, BlockRegistry.CARDBOARD_BOX.getPlacementState(new ItemPlacementContext(context)), 32);
                     world.playSound(null, pos, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, SoundCategory.BLOCKS, 1F, 1F);
-                    blockEntity = world.getBlockEntity(pos); // refresh block entity
-                    if (blockEntity instanceof CardboardBoxBlockEntity) {
-                        ((CardboardBoxBlockEntity) blockEntity).blockState = blockState;
-                        ((CardboardBoxBlockEntity) blockEntity).nbtCopy = nbtList;
-                        blockEntity.markDirty();
+
+                    blockEntity = Optional.ofNullable(world.getBlockEntity(pos)); // refresh block entity
+                    if(blockEntity.isPresent() && blockEntity.get() instanceof CardboardBoxBlockEntity cardboardBoxBE) {
+                        cardboardBoxBE.blockState = blockState;
+                        cardboardBoxBE.nbtCopy = nbtList;
+                        blockEntity.get().markDirty();
                     }
                     context.getStack().decrement(1);
                     return ActionResult.PASS;
