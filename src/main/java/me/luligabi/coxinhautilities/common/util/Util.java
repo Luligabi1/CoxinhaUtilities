6e668fa3d8fd4c8205da93270612df76ad0d928b
Luiz Feikes <33578169+Luligabi1@users.noreply.github.com>
2024-11-07 03:27:11 -0300

    Readd Potato Batteries' distribute energy

--- src/main/java/me/luligabi/coxinhautilities/common/util/Util.java
+++ src/main/java/me/luligabi/coxinhautilities/common/util/Util.java
@@ -10,6 +10,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.CustomData;
 import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.energy.IEnergyStorage;
 import net.neoforged.neoforge.fluids.FluidStack;
 
 import java.text.NumberFormat;
@@ -18,16 +20,6 @@ import java.util.function.Predicate;
 
 public class Util {
 
-    // Converts Fabric's droplets into Forge's milliBuckets for easier displaying of liquid amounts.
-    public static String getMilliBuckets(long dropletAmount) {
-        if (dropletAmount == 0L) {
-            return "0";
-        } else if (dropletAmount < 81) {
-            return "< 1";
-        } else {
-            return "" + dropletAmount / 81;
-        }
-    }
 
     public static FluidStack getFluidFromNbt(CompoundTag nbt) {
         ResourceLocation id;
@@ -48,46 +40,26 @@ public class Util {
         return component.copyTag();
     }
 
-    /*
-     * This code is derivative of the one found in Tech Reborn, copyrighted by Team Reborn and licensed under MIT.
-     *
-     * You may see the original code here: https://github.com/TechReborn/TechReborn/blob/33da2ad59e625cdbd43624635adc65ea7bd23aa5/RebornCore/src/main/java/reborncore/common/util/ItemUtils.java#L246
-     */
-    public static void distributePowerToInventory(Player player, ItemStack itemStack, long maxOutput, Predicate<ItemStack> filter) {
-        /*PlayerInventoryStorage playerInv = PlayerInventoryStorage.of(player);
-        SingleSlotStorage<ItemVariant> sourceSlot = null;
+    public static void distributePowerToInventory(Player player, ItemStack startStack, int maxOutput, Predicate<ItemStack> filter) {
+        IEnergyStorage startStorage = startStack.getCapability(Capabilities.EnergyStorage.ITEM);
+        if(startStorage == null) return;
 
         for(int i = 0; i < player.getInventory().getContainerSize(); i++) {
-            if (player.getInventory().getItem(i) == itemStack) {
-                sourceSlot = playerInv.getSlots().get(i);
-                break;
-            }
+            ItemStack endStack = player.getInventory().getItem(i);
+            if(endStack.isEmpty() || !filter.test(endStack)) continue;
+
+            IEnergyStorage endStorage = endStack.getCapability(Capabilities.EnergyStorage.ITEM);
+            if(endStorage == null) continue;
+
+            int newAmount = Math.min(startStorage.extractEnergy(maxOutput, true), endStorage.receiveEnergy(maxOutput, true));
+            endStorage.receiveEnergy(newAmount, false);
+            startStorage.extractEnergy(newAmount, false);
         }
-
-        if(sourceSlot == null) throw new IllegalArgumentException("Failed to locate current stack in the player inventory.");
-
-        EnergyStorage sourceStorage = ContainerItemContext.ofPlayerSlot(player, sourceSlot).find(EnergyStorage.ITEM);
-        if(sourceStorage == null) return;
-
-        for(int i = 0; i < player.getInventory().getContainerSize(); i++) {
-            ItemStack invStack = player.getInventory().getItem(i);
-
-            if(invStack.isEmpty() || !filter.test(invStack)) continue;
-
-            EnergyStorageUtil.move(
-                    sourceStorage,
-                    ContainerItemContext.ofPlayerSlot(player, playerInv.getSlots().get(i)).find(EnergyStorage.ITEM),
-                    maxOutput,
-                    null
-            );
-        }*/
     }
-
 
     public static ItemStack singleCopy(ItemStack stack) {
         return stack.transmuteCopy(stack.getItem(), 1);
     }
-
 
     /**
      * Used to apply commas and periods to numbers according to the client's language
